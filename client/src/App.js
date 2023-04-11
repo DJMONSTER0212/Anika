5489948cbffe1acb67895ec864d0e5caf7a52391
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2023-04-11 23:14:29 +0530

    new start

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 function App() {
   return (
+    <div className="App">
     <h1>Hello</h1>
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/signup" element={<Signup/>} />
+        {/* <Route exact path="/complaint" element={<ComplaintForm />} /> */}
+    </Routes>
+    </div>
   );
 }
 
